3020ebd39842097a9b4e48347f8abfd8996b8cb3
Catherine <catherine@Chuns-MacBook-Pro-2.local>
2015-05-12 19:02:41 -0400

    Testing protocol

---
+++ src/edu/mit/anonauth/ProtocolSecret.java
@@ -0,0 +1,88 @@
+package edu.mit.anonauth;
+
+import java.io.IOException;
+import java.lang.Math;
+import java.lang.ref.WeakReference;
+import java.util.Arrays;
+import java.util.Collections; 
+import java.util.HashMap; 
+import java.util.Random;
+
+public class ProtocolSecret {
+    private byte[] secretHash;
+    private byte[] challenge;
+    private List<SecretBox> secrets;  // List of polynomials.  Index corresponds to poly degree.  
+    private HashMap<String, List<Point>> userPoints = new HashMap<String, List<Point>>();  
+    // Hashes username to their list of private points/secret shares
+    private List<Point> publicPoints;   // current public points, reset w/ revoke.  
+    private List<String> blacklist;  // List of usernames revoked.  
+    private int polyDegree;  // k = current degree of polynomial
+    private static int maxPolyDegree = 2;  // r = max. degree of polynomial
+    
+    public ProtocolSecret() {
+        setUpScheme();
+    }
+    
+    public void setUpScheme() {
+        polyDegree = 1;
+        // maxPolyDegree = 2;
+        secrets = new ArrayList<SecretBox>();
+        // One-time run to hardcode the boxes.
+        List<BigInteger> coeffs1 = randomSecretBox(1).getCoefficients; 
+        List<BigInteger> coeffs2 = randomSecretBox(2).getCoefficients;
+        System.println(coeffs1);
+        System.println(coeffs2);
+        SecretBox box1 = fromCoefficients(coeffs1);
+        SecretBox box2 = fromCoefficients(coeffs2);
+
+        // How do we initialize the secret hash and challenge?  
+        secretHash = box1.secretHash();  // Must be 32 bytes
+        // challenge = new byte[16];
+        challenge = cleanByteArray(new BigInteger(16, rand), 16);
+    }
+    
+    /* 
+    Format of the command:
+    1 byte = k (# points)
+    y = 16 bytes
+    x = 2 bytes
+    Hash of secret - 32 bytes
+    Challenge - 16 bytes
+    */
+    public byte[] getBroadcast(){
+        byte[] command = new byte[]{};
+        command[0] = (byte) polyDegree;  // k = 1;
+        for (int i = 0; i < polyDegree; i++) {
+            // Need k points in command.
+            Point p = publicPoints.get(i);
+            byte[] xBytes = cleanByteArray(p.x, 2);
+            byte[] yBytes = cleanByteArray(p.y, 16);
+            System.arraycopy(xBytes, 0, command, 1 + i*18, 2);
+            System.arraycopy(yBytes, 0, command, 3 + i*18, 16);
+        }
+        System.arraycopy(secretHash, polyDegree*32, command, 1 + polyDegree*18, 32);
+        System.arraycopy(challenge, polyDegree*16, command, 1 + polyDegree*18 + 32, 16);
+        return command;
+    }
+
+    /* Returns bigInteger in byte array form of fixed length.  Assumes positive bigInteger */
+    public byte[] cleanByteArray(BigInteger bigInteger, int fixedLength) {
+        // From http://stackoverflow.com/questions/4407779/biginteger-to-byte
+        // If we're only dealing with positive x and y values, 
+        // can get rid of leading sign bit to keep array at size 2 and 16 bytes, respectively.  
+        byte[] array = bigInteger.toByteArray();
+        if (array[0] == 0) {
+            byte[] tmp = new byte[array.length - 1];
+            System.arraycopy(array, 1, tmp, 0, tmp.length);
+            array = tmp;
+        }
+        byte[] fixedArray = new byte[fixedLength];
+        System.arraycopy(array, 0, fixedArray, fixedLength - array.size(), array.size())
+        return fixedArray;
+    }
+    
+    public boolean matchesHMAC(byte[] response) {
+        return (box1.hmac(challenge)).equals(response);
+    }
+
+}
---
+++ src/edu/mit/anonauth/ProtocolSecretTest.java
@@ -0,0 +1,50 @@
+package edu.mit.anonauth;
+
+public class ProtocolSecretTest extends ProtocolSecret {
+    
+    @Test
+    public void test() {
+        byte[] command = getBroadcast();
+        byte[] response = parseBroadcast(command);
+        assertTrue(matchesHMAC(response));
+    }
+    
+    /* Card's parsing and response protocol. */
+    public byte[] parseBroadcast(byte[] broadcast){
+     // k |( x | y )*k | hashSecret | challenge (max 255)
+                 Byte kByte = broadcast[0];
+                 int k = kByte.intValue();
+                 List<Point> points = new ArrayList<Point>();
+                 byte[] hmac = new byte[]{};
+
+                 int hashBegin = 1 + k * 18;
+                 int challengeBegin = hashBegin + 32;
+
+                 byte[] hashedSecret = Arrays.copyOfRange(broadcast, hashBegin, hashBegin + 32); //32 bytes
+                 BigInteger challenge = new BigInteger(Arrays.copyOfRange(broadcast, hashBegin + 32, hashBegin + 48)); //16 bytes
+
+                 points.add(PRIVATE_POINTS[k]);
+
+                 for (int i = 0; i < k; i++){
+                     int pointStart = 1 + i * 18;
+                     BigInteger x = new BigInteger(Arrays.copyOfRange(broadcast, pointStart, pointStart + 2)); //2 bytes
+                     BigInteger y = new BigInteger(Arrays.copyOfRange(broadcast, pointStart + 2, pointStart + 18)); //16 bytes
+                     Point xy = new Point(x, y);
+
+                     points.add(xy);
+                 }
+
+                 SecretBox secretBox = SecretBox.fromPoints(points);
+
+//                 BigInteger secretPassword = CryptoLibrary.Shamir(points);
+                 byte[] hashedSecretPassword = secretBox.secretHash();
+
+                 if (hashedSecretPassword.equals(hashedSecret)){
+     //Not a fake door
+                     hmac = secretBox.hmac(challenge);
+                 }
+
+                 return hmac;
+         }
+
+}
