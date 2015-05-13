4814c0f2089598ec4ecce8f1af0c929ce5cb6d09
btidor <btidor@mit.edu>
2015-05-13 15:40:11 -0400

    Add more complex protocol tests

--- src/edu/mit/anonauth/ProtocolDoorTest.java
+++ src/edu/mit/anonauth/ProtocolDoorTest.java
@@ -9,15 +9,58 @@ public class ProtocolDoorTest extends ProtocolDoor {
 	public ProtocolDoorTest() { super(2); }
 	
     @Test
-    public void testBasicExchange() {
+    public void testBasicExchangeSmallR() {
     	int r = 5;
-    	int user = 101;
+    	int user = 5001;
+    	
+    	ProtocolDoor door = new ProtocolDoor(r);
+    	ProtocolCard card = new ProtocolCard(door.privatePoints(user));
+    	assertTrue(exchange(door, card));
+    }
+    
+    @Test
+    public void testBasicExchangeLargeR() {
+    	int r = 500;
+    	int user = 5001;
+    	
+    	ProtocolDoor door = new ProtocolDoor(r);
+    	ProtocolCard card = new ProtocolCard(door.privatePoints(user));
+    	assertTrue(exchange(door, card));
+    }
+    
+    @Test
+    public void testRevocation() {
+    	int r = 5;
+    	int user = 5001;
     	
     	ProtocolDoor door = new ProtocolDoor(r);
     	ProtocolCard card = new ProtocolCard(door.privatePoints(user));
     	
+    	assertTrue(exchange(door, card));
+    	door.revoke(user);
+    	assertFalse(exchange(door, card));
+    }
+    
+    @Test
+    public void testRevocationMultiUser() {
+    	int r = 5;
+    	int userA = 5001;
+    	int userB = 5002;
+    	
+    	ProtocolDoor door = new ProtocolDoor(r);
+    	ProtocolCard cardA = new ProtocolCard(door.privatePoints(userA));
+    	ProtocolCard cardB = new ProtocolCard(door.privatePoints(userB));
+    	
+    	assertTrue(exchange(door, cardA));
+    	assertTrue(exchange(door, cardB));
+    	door.revoke(userB);
+    	assertTrue(exchange(door, cardA));
+    	assertFalse(exchange(door, cardB));
+    }
+    
+    public boolean exchange(ProtocolDoor door, ProtocolCard card) {
     	byte[] broadcast = door.getBroadcast();
     	byte[] response = card.authenticate(broadcast);
-    	assertTrue(door.checkResponse(response));
+    	return door.checkResponse(response);
     }
 }
